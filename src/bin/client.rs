1f0088d02d46372b720694feb90277af31434a86
赵光建 <zhaoguangjian@cyberkl.com>
2024-12-05 00:59:12 +0800

    init client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("client");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().with_line_number(true).init();
+    log::info!("Client is running...");
+    Ok(())
 }
